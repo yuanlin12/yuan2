af328d749b9d582e260a8b6314ee9ce9ba380a25
yuanlin12 <416306434@qq.com>
2017-04-17 20:17:59 +0800

    123

---
+++ src/main/java/com/yl/project1/App.java
@@ -0,0 +1,13 @@
+package com.yl.project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
